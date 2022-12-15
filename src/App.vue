b93e1ae017d1c655ddd36f712c86fe8b881408db
Federico <fede.toldo96@gmail.com>
2022-12-15 19:32:12 +0100

    fix style position
    fix style position

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,9 @@ import TodoInput from './components/TodoInput.vue';
 </script>
 
 <template>
+  <div>
+    <h1>Todo List</h1>
+    <h5>By Federico Toldo</h5>
+  </div>
   <TodoInput/>
 </template>
-
-<style lang="scss">
-
-  body {
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    background: linear-gradient(to bottom right, #99C191, #C3D98D);
-  }
-  #app {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 1rem;
-    color: #333;
-    width: 100%;
-    max-width: 768px;
-  }
-
-</style>
